037a9190848ecfa59f6ec328f6549c16f74d2c82
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-23 16:24:36 -0700

    add UI logic for the places

--- js/scripts.js
+++ js/scripts.js
@@ -39,17 +39,37 @@ function Place(location, landmarks, timeOfYear, notes) {
 
 //UI logic
 
+let vacations = new MyVacations();
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
     event.preventDefault();
+    $("ul#places").empty();
     const locationInput = $("#locationInput").val();
     const landmarksInput = $("#landmarksInput").val();
     const timeInput = $("#timeInput").val();
     const notesInput = $("#notesInput").val();
 
+    const place = new Place(locationInput, landmarksInput, timeInput, notesInput);
+
+    vacations.addPlace(place);
+    let testVar = vacations.places;
+
+    console.log(typeof(testVar));
+    
     $(".location").text(locationInput);
     $(".landmarks").text(landmarksInput);
     $(".time-of-year").text(timeInput);
     $(".notes").text(notesInput);
+
+    for ( i in testVar ){
+      $("ul#places").append("<li>" + testVar[i].location + "</li>");
+    }
+
+    for ( i in testVar ){
+      $("ol#placesLand").append("<li>" + testVar[i].landmarks + "</li>");
+    }
+
+    //$("ul#places").append("<li>" + "to be relaced" + "</li>");
+
   });
 });
